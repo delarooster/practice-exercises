965238dcfe26a447f4573daf54017945f4197bcd
Austin DeLaRosa <276900@djjgx.biz>
2020-03-14 14:53:39 -0400

    Adding gitignore and a few starter methods to pattern

--- PracticeTechElevatorExercises/PracticeTechElevatorExercises/Program.cs
+++ PracticeTechElevatorExercises/PracticeTechElevatorExercises/Program.cs
@@ -4,10 +4,23 @@ namespace PracticeTechElevatorExercises
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            static void Exercise1()
+            {
+                Console.WriteLine("Tom needs a shower!");
+                Console.ReadKey();
+            }
+
+            Exercise1(); //calling Exercise1 method
+
+            static void Exercise2(string name)
+            {
+                Console.WriteLine(name + " needs a shower!");
+                Console.ReadKey();
+            }
+
+            Exercise2("Irving"); //calling Exercise2 method
         }
     }
 }
