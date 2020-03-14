532e0e948b6ae90cb045a0c038f9407f69220b14
Austin DeLaRosa <276900@djjgx.biz>
2020-03-14 15:58:07 -0400

    Added solution to exercises 3

--- PracticeTechElevatorExercises/Elementary.cs
+++ PracticeTechElevatorExercises/Elementary.cs
@@ -6,12 +6,30 @@ public class Elementary
 {
     public static void Exercise1()
     {
-        Console.WriteLine("Tom needs a shower!");
+        Console.WriteLine("Hello world!");
         Console.ReadKey();
     }
-    public static void Exercise2(string name)
+    public static void Exercise2()
     {
-        Console.WriteLine(name + " needs a shower!");
+        Console.WriteLine("Please write your first name: ");
+        string name = Console.ReadLine().Trim();
+        Console.WriteLine($"Hello {name}!");
+        Console.ReadKey();
+    }
+    public static void Exercise3()
+    {
+        Console.WriteLine("Please write your first name: ");
+        string name = Console.ReadLine().Trim();
+
+        if (name == "Bob" || name == "Alice")
+        {
+            Console.WriteLine($"Hello {name}!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you're not allowed to be greeted by name!");
+        }
+
         Console.ReadKey();
     }
 }
--- PracticeTechElevatorExercises/Program.cs
+++ PracticeTechElevatorExercises/Program.cs
@@ -5,7 +5,8 @@ class Program
 {
     static void Main()
     {
-        Exercise1(); //calling Exercise1 method
-        Exercise2("Irving"); //calling Exercise2 method
+        //Exercise1(); //calling Exercise1 method
+        //Exercise2(); //calling Exercise2 method
+        Exercise3();
     }
 }
