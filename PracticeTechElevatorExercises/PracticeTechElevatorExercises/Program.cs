0cece19dfaa1ff92a49d3a588cc51765591f24c6
Austin DeLaRosa <276900@djjgx.biz>
2020-03-14 13:14:48 -0400

    initial commit

---
+++ PracticeTechElevatorExercises/PracticeTechElevatorExercises/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PracticeTechElevatorExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
